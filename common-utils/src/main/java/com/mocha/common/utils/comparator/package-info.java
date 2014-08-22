71e63a218ea177d311bf5d7044af5046f6bbafb4
lewjeen <lewjeen@163.com>
2014-08-22 14:05:55 +0800

    提交

---
+++ common-utils/src/main/java/com/mocha/common/utils/comparator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * Useful generic <code>java.util.Comparator</code> implementations,
+ * such as an invertible comparator and a compound comparator.
+ *
+ */
+package com.mocha.common.utils.comparator;
+
---
+++ common-utils/src/main/java/com/mocha/common/utils/xml/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * Miscellaneous utility classes for XML parsing and transformation,
+ * such as error handlers that log warnings via Commons Logging.
+ *
+ */
+package com.mocha.common.utils.xml;
+
